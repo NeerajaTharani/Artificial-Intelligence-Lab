0bd60f04e3513bcd0cdb73c403ffef0935e4a9d0
NeerajaTharani <90817823+NeerajaTharani@users.noreply.github.com>
2022-04-27 20:10:10 +0530

    Update and rename Depth First Search to DFS.java

--- DFS.java
+++ DFS.java
@@ -3,9 +3,9 @@ import java.util.*;
  
 
 class Graph {
-    private int V;                              //number of nodes
+    private int V;                              
  
-    private LinkedList<Integer> adj[];              //adjacency list
+    private LinkedList<Integer> adj[];             
  
     public Graph(int v)
     {
@@ -18,21 +18,21 @@ class Graph {
  
     void addEdge(int v, int w)
     {
-        adj[v].add(w);                              //adding an edge to the adjacency list (edges are bidirectional in this example)
+        adj[v].add(w);                              
     }
  
 
     void DFSUtil(int vertex, boolean nodes[])
     {
 
-        nodes[vertex] = true;                         //mark the node as explored
+        nodes[vertex] = true;                         
         System.out.print(vertex + " ");
         int a = 0;
  
-        for (int i = 0; i < adj[vertex].size(); i++)  //iterate through the linked list and then propagate to the next few nodes
+        for (int i = 0; i < adj[vertex].size(); i++)  
             {
                 a = adj[vertex].get(i);
-                if (!nodes[a])                    //only propagate to next nodes which haven't been explored
+                if (!nodes[a])                    
                 {
                     DFSUtil(a, nodes);
                 }
@@ -41,7 +41,7 @@ class Graph {
 
     void DFS(int v)
     {
-        boolean already[] = new boolean[V];             //initialize a new boolean array to store the details of explored nodes
+        boolean already[] = new boolean[V];             
         DFSUtil(v, already);
     }
  
@@ -75,9 +75,9 @@ import java.util.*;
 
  class Graph 
     { 
-        int V;                                          //number of nodes
+        int V;                                          
           
-        LinkedList<Integer>[] adj;                      //adjacency list
+        LinkedList<Integer>[] adj;                      
           
         Graph(int V) 
         { 
@@ -91,7 +91,7 @@ import java.util.*;
           
         void addEdge(int v, int w) 
         { 
-            adj[v].add(w);                              //adding an edge to the adjacency list (edges are bidirectional in this example)
+            adj[v].add(w);                             
         } 
           
         void DFS(int n) 
@@ -100,13 +100,13 @@ import java.util.*;
       
             Stack<Integer> stack = new Stack<>(); 
               
-            stack.push(n);                                    //push root node to the stack
+            stack.push(n);                                    
             int a = 0;
               
             while(!stack.empty()) 
             { 
-                n = stack.peek();                       //extract the top element of the stack
-                stack.pop();                            //remove the top element from the stack
+                n = stack.peek();                      
+                stack.pop();                            
                   
                 if(nodes[n] == false) 
                 { 
@@ -114,12 +114,12 @@ import java.util.*;
                     nodes[n] = true; 
                 } 
                   
-                for (int i = 0; i < adj[n].size(); i++)  //iterate through the linked list and then propagate to the next few nodes
+                for (int i = 0; i < adj[n].size(); i++) 
             {
                 a = adj[n].get(i);
-                if (!nodes[a])                    //only push those nodes to the stack which aren't in it already
+                if (!nodes[a])                   
                 {
-                    stack.push(a);                          //push the top element to the stack
+                    stack.push(a);                          
                 }
             }  
                   
